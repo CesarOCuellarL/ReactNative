506fc8518a10409b982c28cec9cfb2ae7465911a
Ceslusbel <ceslusbel@gmail.com>
2022-03-26 21:27:19 -0600

    Cambio graphql

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,150 +1,32 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getBlog = /* GraphQL */ `
-  query GetBlog($id: ID!) {
-    getBlog(id: $id) {
-      id
-      name
-      posts {
-        items {
-          id
-          title
-          createdAt
-          updatedAt
-          blogPostsId
-        }
-        nextToken
-      }
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const listBlogs = /* GraphQL */ `
-  query ListBlogs(
-    $filter: ModelBlogFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listBlogs(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        name
-        posts {
-          nextToken
-        }
-        createdAt
-        updatedAt
-      }
-      nextToken
-    }
-  }
-`;
-export const getPost = /* GraphQL */ `
-  query GetPost($id: ID!) {
-    getPost(id: $id) {
+export const getTodo = /* GraphQL */ `
+  query GetTodo($id: ID!) {
+    getTodo(id: $id) {
       id
       title
-      blog {
-        id
-        name
-        posts {
-          nextToken
-        }
-        createdAt
-        updatedAt
-      }
-      comments {
-        items {
-          id
-          content
-          createdAt
-          updatedAt
-          postCommentsId
-        }
-        nextToken
-      }
+      author
+      ISBN
       createdAt
       updatedAt
-      blogPostsId
     }
   }
 `;
-export const listPosts = /* GraphQL */ `
-  query ListPosts(
-    $filter: ModelPostFilterInput
+export const listTodos = /* GraphQL */ `
+  query ListTodos(
+    $filter: ModelTodoFilterInput
     $limit: Int
     $nextToken: String
   ) {
-    listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
       items {
         id
         title
-        blog {
-          id
-          name
-          createdAt
-          updatedAt
-        }
-        comments {
-          nextToken
-        }
+        author
+        ISBN
         createdAt
         updatedAt
-        blogPostsId
-      }
-      nextToken
-    }
-  }
-`;
-export const getComment = /* GraphQL */ `
-  query GetComment($id: ID!) {
-    getComment(id: $id) {
-      id
-      post {
-        id
-        title
-        blog {
-          id
-          name
-          createdAt
-          updatedAt
-        }
-        comments {
-          nextToken
-        }
-        createdAt
-        updatedAt
-        blogPostsId
-      }
-      content
-      createdAt
-      updatedAt
-      postCommentsId
-    }
-  }
-`;
-export const listComments = /* GraphQL */ `
-  query ListComments(
-    $filter: ModelCommentFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listComments(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        post {
-          id
-          title
-          createdAt
-          updatedAt
-          blogPostsId
-        }
-        content
-        createdAt
-        updatedAt
-        postCommentsId
       }
       nextToken
     }
--- src/services/todos.js
+++ src/services/todos.js
@@ -1,87 +1,35 @@
 import { API, graphqlOperation } from "aws-amplify";
-import { getBlog,getComment,getPost,listBlogs,listPosts,listComments } from "../graphql/queries";
-import { createBlog,createComment,createPost,updateBlog,updateComment,updatePost } from "../graphql/mutations";
-import {onCreateBlog,onCreateComment,onCreatePost} from "../graphql/subscriptions";
+import { getTodo, listTodos } from "../graphql/queries";
+import { createTodo, updateTodo, deleteTodo } from "../graphql/mutations";
+import { onCreateTodo } from "../graphql/subscriptions";
 
-const listBlogs = async() => {
+const list = async() => {
     try{
-        const blogs = await API.graphql(graphqlOperation(listBlogs));
-        return blogs.data.listBlogs.items;
+        const todos = await API.graphql(graphqlOperation(listTodos));
+        return todos.data.listTodos.items;
     }catch(error){
-
+        console.log({ error });
     }
 }
 
-const listComments = async() => {
-    try{
-        const comments = await API.graphql(graphqlOperation(listComments));
-        return comments.data.listComments.items;
-    }catch(error){
-
-    }
-}
-
-const listPosts = async() => {
-    try{
-        const posts = await API.graphql(graphqlOperation(listPosts));
-        return posts.data.listPosts.items;
-    }catch(error){
-        console.log({error});
-    }
-}
-
-const createBlog = async(Blog)=>{
+const create = async(todo)=>{
     try {
-        const newBlog = await API.graphql(graphqlOperation(createBlog,{input: Blog}));
-        return newBlog;
+        const newTodo = await API.graphql(graphqlOperation(createTodo,{input: todo}));
+        return newTodo;
     } catch (error) {
         console.log({error})
     }
 }
 
-const createComment = async(Comment)=>{
-    try {
-        const newComment = await API.graphql(graphqlOperation(createComment,{input: Comment}));
-        return newComment;
-    } catch (error) {
-        console.log({error})
-    }
-}
-
-const createPost = async(Post)=>{
-    try {
-        const newPost = await API.graphql(graphqlOperation(createPost,{input: Post}));
-        return newPost;
-    } catch (error) {
-        console.log({error})
-    }
-}
-
-const onCreateBlog = async(subscriptionFunction) => {
-    const subscription = API.graphql(graphqlOperation(onCreateBlog)).suscribe({
-        next:(todoBlog) => {
+const onCreate = async(subscriptionFunction) => {
+    const subscription = API.graphql(graphqlOperation(onCreateTodo)).suscribe({
+        next:(todoData) => {
             subscriptionFunction();
         },
     });
     return subscription;
 }
 
-const onCreateComment = async(subscriptionFunction) => {
-    const subscription = API.graphql(graphqlOperation(onCreateComment)).suscribe({
-        next:(todoComment) => {
-            subscriptionFunction();
-        },
-    });
-    return subscription;
-}
 
-const onCreatePost = async(subscriptionFunction) => {
-    const subscription = API.graphql(graphqlOperation(onCreatePost)).suscribe({
-        next:(todoPost) => {
-            subscriptionFunction();
-        },
-    });
-    return subscription;
-};
 
-export {listBlogs,listComments,listPosts,createBlog,createComment,createPost,onCreateBlog,onCreateComment,onCreatePost};
+export {list,create,onCreate};
