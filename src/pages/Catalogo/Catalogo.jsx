06e5a9e3b2726935c3cd0331825cf9f41b2430c4
afrancog1996 <alejandrofrancog95@gmail.com>
2022-04-05 22:56:41 -0500

    fix: map key component

--- src/pages/Catalogo/Catalogo.jsx
+++ src/pages/Catalogo/Catalogo.jsx
@@ -1,22 +1,32 @@
-import React, {useEffect, useContext} from "react";
-import { Text, View } from 'react-native';
+import React, { useEffect, useContext } from "react";
+import { Text, View } from "react-native";
 import { styles } from "./Catalogo.styles";
-import {list} from "../../services/Books";
+import { list } from "../../services/Books";
 
 import { GlobalContext } from "../../context/global/global.context";
 
 export default function CatalogoScreen() {
-    const {listBooks, books} = useContext(GlobalContext);
+  const { listBooks, books } = useContext(GlobalContext);
 
-    useEffect(() => {
-        listBooks();
-    },[])
-    
-    return (
-        <View style={styles.container}>
-            {books && books.map((book)=> 
-            <><Text>Name: {`${book.Name}`}</Text><Text>Description: {`${book.Description} `}</Text><Text>Estatus: {`${book.Estatus} `}</Text><Text>ISBN: {`${book.ISBN}`}</Text><Text>Categoria: {`${book.Categoria}`}</Text><Text style={{marginBottom: 30}}>Fecha de Publicacion: {`${book.FechaPublicacion}`}</Text></>            
-            )}
-        </View>
-    );
+  useEffect(() => {
+    listBooks();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {books &&
+        books.map((book, i) => (
+          <View key={i}>
+            <Text>Name: {`${book.Name}`}</Text>
+            <Text>Description: {`${book.Description} `}</Text>
+            <Text>Estatus: {`${book.Estatus} `}</Text>
+            <Text>ISBN: {`${book.ISBN}`}</Text>
+            <Text>Categoria: {`${book.Categoria}`}</Text>
+            <Text style={{ marginBottom: 30 }}>
+              Fecha de Publicacion: {`${book.FechaPublicacion}`}
+            </Text>
+          </View>
+        ))}
+    </View>
+  );
 }
